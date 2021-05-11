94a8eec90fe9b96d83b8e82dc612ee919b7c3df5
Deng.Zixu <admin@dengzixu.com>
2021-05-11 21:38:38 +0800

    支持礼物信息获取粉丝牌
    + 支持礼物信息获取粉丝牌
    + 支持礼物信息获取用户头像

--- src/main/java/net/dengzixu/java/payload/body/resolver/SendGiftResolver.java
+++ src/main/java/net/dengzixu/java/payload/body/resolver/SendGiftResolver.java
@@ -32,6 +32,7 @@ public class SendGiftResolver extends BodyResolver {
             sendGiftBody.setUserInfo(new UserInfo() {{
                 setUsername((String) dataMap.get("uname"));
                 setUid((int) dataMap.get("uid"));
+                setFace((String) dataMap.get("face"));
             }});
 
             sendGiftBody.setCoinType((String) dataMap.get("coin_type"));
@@ -41,9 +42,20 @@ public class SendGiftResolver extends BodyResolver {
             sendGiftBody.setNum((int) dataMap.get("num"));
             sendGiftBody.setPrice((int) dataMap.get("price"));
 
-            sendGiftBody.setFansMedal(new FansMedal() {{
+            // 粉丝牌信息
+            final Map<String, Object> fansMedalMap = (Map<String, Object>) dataMap.get("medal_info");
 
-            }});
+            if ((int) fansMedalMap.get("target_id") != 0) {
+                sendGiftBody.setFansMedal(new FansMedal() {{
+                    setMedalLevel((int) fansMedalMap.get("medal_level"));
+                    setMedalName((String) fansMedalMap.get("medal_name"));
+                    setMedalColor((int) fansMedalMap.get("medal_color"));
+                    setMedalColorBorder((int) fansMedalMap.get("medal_color_border"));
+                    setMedalColorStart((int) fansMedalMap.get("medal_color_start"));
+                    setMedalColorEnd((int) fansMedalMap.get("medal_color_end"));
+                    setLighted((int) fansMedalMap.get("is_lighted") == 1);
+                }});
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
