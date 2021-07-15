cdb6bd8e12fa101d8ef3a168c5b4290031351c36
Deng.Zixu <admin@dengzixu.com>
2021-07-15 17:05:37 +0800

    支持 Lambda 表达式

--- src/main/java/net/dengzixu/java/listener/Listener.java
+++ src/main/java/net/dengzixu/java/listener/Listener.java
@@ -2,6 +2,6 @@ package net.dengzixu.java.listener;
 
 import net.dengzixu.java.message.Message;
 
-public abstract class Listener {
-    public abstract void onMessage(Message message);
+public interface Listener {
+    void onMessage(Message message);
 }
